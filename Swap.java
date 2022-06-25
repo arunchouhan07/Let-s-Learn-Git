9f56e71efbf1d2aa8cdf23dbbc7ae65e4dbf9c78
Arun Chouhan <arunchouhan2352@gmail.com>
2022-06-25 21:46:53 +0530

    Swapping Two Number

--- Swap.java
+++ Swap.java
@@ -4,11 +4,11 @@ public class Swap{
         int a=10;
         int b=20;
 
-        System.out.println("Numbers Before Swap "+a+" "+b);
+        System.out.println("Numbers Before Swap a="+a+" b="+b);
         a=a+b; //a=10+20 =30;
         b=a-b; //b=30-20 =10;
         a=a-b; //a=30-10 =20;
         
-        System.out.printf("Numbers After Swap "+a+" "+b);
+        System.out.printf("Numbers After Swap a="+a+" b="+b);
     }
 }
